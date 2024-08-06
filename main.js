6f499ce3f4b680f60a04e3855194b81b1d63ede5
Chemonky <luricardorod@gmail.com>
2024-08-06 12:25:03 -0600

    funcionamiento base

--- main.js
+++ main.js
@@ -1,80 +1,171 @@
-import './style.css'
+/* global fabric */
+let counter = 0
+const updateGroup = () => {
+  console.log(document.getElementById('myText').value)
+  const activeObj = canvas.getActiveObject()
+  const objects = activeObj.getObjects()
+
+  const textObject = objects.find(obj => {
+    console.log(obj.type)
+    return obj.type === 'textbox'
+  })
+
+  if (textObject) {
+    textObject.set('text', document.getElementById('myText').value)
+    canvas.renderAll() // Renderizar el canvas para aplicar los cambios
+  }
+}
+
+const saveJSON = () => {
+  canvas.includeDefaultValues = false
+  console.log(JSON.stringify(canvas.toJSON(), null, '  '))
+}
 
 document.querySelector('#app').innerHTML = `
-  <div>
-    <img
-        class="img"
-        id="ele1"
-        draggable="true"
-        src="https://webkit.org/wp-content/uploads/STP-300x300.png"
-      />
-      <canvas id="c1" width="800" height="600"></canvas>
-      <div id="drag-stuff" class="log">
-        Drag me on the canvas<br /><br />
-        <div id="drag-me" draggable="true"></div>
-      </div>
-  </div>
+  
 `
+const tablesOptions = [
+  {
+    id: 'table1',
+    src: './mesas/Mesa-2.png'
+  },
+  {
+    id: 'table2',
+    src: './mesas/Mesa-4.png'
+  },
+  {
+    id: 'table3',
+    src: './mesas/Mesa-6.png'
+  }
+]
+
+const controls = document.getElementById('controls')
+
+tablesOptions.forEach(option => {
+  const img = document.createElement('img')
+  img.className = 'img-table'
+  img.id = option.id
+  img.draggable = true
+  img.src = option.src
+  img.addEventListener('dragstart', dragElement)
+  controls.appendChild(img)
+})
+
+const blocks = document.getElementsByClassName('rectangleControl')
+Array.from(blocks).forEach(rec => {
+  rec.addEventListener('dragstart', dragElement)
+})
 
 fabric.Object.prototype.transparentCorners = false
 const canvas = new fabric.Canvas('c1')
-canvas.add(
-  new fabric.Rect({
-    width: 50,
-    height: 50,
-    fill: 'green',
-    top: 100,
-    left: 100
-  })
-)
-canvas.add(
-  new fabric.Rect({
-    width: 30,
-    height: 30,
-    fill: 'green',
-    top: 50,
-    left: 50
-  })
-)
-canvas.add(new fabric.Circle({ radius: 20, fill: 'blue', top: 160, left: 140 }))
-canvas.add(new fabric.Textbox('Textbox', { fill: 'black', top: 70, left: 200 }))
 
 function observe (eventName) {
   canvas.on(eventName, function (opt) {
     if (eventName === 'drop') {
       dropElement(opt.e)
     }
+    if (
+      eventName === 'selection:updated' ||
+      eventName === 'selection:created'
+    ) {
+      showSelection(opt)
+    }
     console.log(eventName, opt)
   })
+}
+
+const showSelection = opt => {
+  const activeObj = canvas.getActiveObject()
+  if (activeObj.type !== 'group') return
+  const objects = activeObj.getObjects()
+
+  const textObject = objects.find(obj => {
+    console.log(obj.type)
+    return obj.type === 'textbox'
+  })
+
+  document.getElementById('myText').value = textObject.text
 }
 
 observe('drop:before')
 observe('drop')
 observe('dragenter')
 observe('dragleave')
-// allowDrop function called on ondragover event.
-function allowDrop (e) {
-  e.preventDefault()
-}
-//dragElement function called on ondrag event.
+observe('selection:created')
+observe('selection:updated')
+
+// dragElement function called on ondrag event.
 function dragElement (e) {
-  console.log('asdasda')
-  e.dataTransfer.setData('id', e.target.id) //transfer the "data" i.e. id of the target dragged.
+  console.log(e.target.id)
+  e.dataTransfer.setData('id', e.target.id) // transfer the "data" i.e. id of the target dragged.
+}
+const RECTANGLE_SIZE_CONTROLS = 50
+
+const createRectangle = ({ e, color, type }) => {
+  const rectangle = new fabric.Rect({
+    width: RECTANGLE_SIZE_CONTROLS,
+    height: RECTANGLE_SIZE_CONTROLS,
+    left: e.layerX - RECTANGLE_SIZE_CONTROLS / 2,
+    top: e.layerY - RECTANGLE_SIZE_CONTROLS / 2,
+    fill: color
+  })
+
+  const text = new fabric.Textbox(type + counter.toString(), {
+    fontSize: 10,
+    left: e.layerX - RECTANGLE_SIZE_CONTROLS / 2,
+    width: RECTANGLE_SIZE_CONTROLS,
+    top: e.layerY - 10 - RECTANGLE_SIZE_CONTROLS / 2,
+    textAlign: 'center'
+  })
+  text.set('lockScalingX', true)
+  counter++
+  const group = new fabric.Group([rectangle, text])
+  // group.on('scaling', function () {
+  //   console.log('asdasda')
+  //   console.log(group)
+  //   const scaleX = group.scaleX
+  //   const scaleY = group.scaleY
+  //   // Escalar solo el rectángulo
+  //   group.item(1).set({
+  //     scaleX: 1 / scaleX,
+  //     scaleY: 1 / scaleY
+  //   })
+
+  //   group.setCoords()
+  //   canvas.renderAll()
+  // })
+  canvas.add(group)
 }
 
-//dropElement function called on ondrop event.
+// dropElement function called on ondrop event.
 function dropElement (e) {
   e.preventDefault()
-  var data = e.dataTransfer.getData('id') //receiving the "data" i.e. id of the target dropped.
-  var imag = document.getElementById(data) //getting the target image info through its id.
-  var img = new fabric.Image(imag, {
-    //initializing the fabric image.
-    left: e.layerX - 80, //positioning the target on exact position of mouse event drop through event.layerX,Y.
-    top: e.layerY - 40
-  })
-  img.scaleToWidth(imag.width) //scaling the image height and width with target height and width, scaleToWidth, scaleToHeight fabric inbuilt function.
-  img.scaleToHeight(imag.height)
-  canvas.add(img)
-}
+  const id = e.dataTransfer.getData('id')
 
-document.getElementById('ele1').addEventListener('dragstart', dragElement)
+  if (id === 'area') {
+    createRectangle({ e, color: 'rgba(0, 128, 0, 0.1)', type: 'Area' })
+    return
+  }
+  if (id === 'floor') {
+    createRectangle({ e, color: 'rgba(0, 0, 255, 0.1)', type: 'Piso' })
+    return
+  }
+  const imgData = document.getElementById(id)
+  const width = imgData.width
+  const height = imgData.height
+
+  const img = new fabric.Image(imgData, {
+    left: e.layerX - width / 2,
+    top: e.layerY - height / 2
+  })
+  const text = new fabric.Textbox(counter.toString(), {
+    fontSize: 10,
+    left: e.layerX - width / 2,
+    width,
+    top: e.layerY - 5,
+    textAlign: 'center'
+  })
+  counter++
+  const group = new fabric.Group([img, text])
+  canvas.add(group)
+}
