8cd2661da15a5c486ecbe12b379d3ca429973c42
Chemonky <luricardorod@gmail.com>
2024-08-08 10:23:41 -0600

    uso svg

--- main.js
+++ main.js
@@ -1,18 +1,42 @@
 /* global fabric */
 let counter = 0
-const updateGroup = () => {
-  console.log(document.getElementById('myText').value)
-  const activeObj = canvas.getActiveObject()
-  const objects = activeObj.getObjects()
+const FONT_SIZE_AREA = 20
+const FONT_SIZE_FLOOR = 25
+const RECTANGLE_SIZE_CONTROLS = 50
+const ZONE_TYPE = {
+  area: 'AREA',
+  floor: 'PISO',
+  table: 'TABLE'
+}
+const FONT_SIZE_TABLES = 15
+const TABLES_OPTIONS = [
+  {
+    id: 'table1',
+    color: 'rgba(0, 128, 0, 0.6)',
+    name: 'Alta',
+    src: './mesas/android.svg'
+  },
+  {
+    id: 'table2',
+    color: 'rgba(128, 128, 0, 0.6)',
+    name: 'Sala',
+    src: './mesas/android.svg'
+  },
+  {
+    id: 'table3',
+    color: 'rgba(0, 128, 128, 0.6)',
+    name: 'Barra',
+    src: './mesas/android.svg'
+  }
+]
 
-  const textObject = objects.find(obj => {
-    console.log(obj.type)
-    return obj.type === 'textbox'
-  })
+const updateGroup = () => {
+  const activeObj = canvas.getActiveObject()
+  const textObject = activeObj.textObject
 
   if (textObject) {
     textObject.set('text', document.getElementById('myText').value)
-    canvas.renderAll() // Renderizar el canvas para aplicar los cambios
+    canvas.renderAll()
   }
 }
 
@@ -21,41 +45,52 @@ const saveJSON = () => {
   console.log(JSON.stringify(canvas.toJSON(), null, '  '))
 }
 
-document.querySelector('#app').innerHTML = `
-  
-`
-const tablesOptions = [
-  {
-    id: 'table1',
-    src: './mesas/Mesa-2.png'
-  },
-  {
-    id: 'table2',
-    src: './mesas/Mesa-4.png'
-  },
-  {
-    id: 'table3',
-    src: './mesas/Mesa-6.png'
-  }
-]
+function loadAndChangeSVGColor (url, color, id) {
+  fetch(url)
+    .then(response => {
+      console.log(response)
+      return response.text()
+    })
+    .then(svgText => {
+      console.log(svgText)
 
-const controls = document.getElementById('controls')
+      // Crear un contenedor temporal para el SVG
+      const tempContainer = document.createElement('div')
 
-tablesOptions.forEach(option => {
-  const img = document.createElement('img')
-  img.className = 'img-table'
-  img.id = option.id
-  img.draggable = true
-  img.src = option.src
-  img.addEventListener('dragstart', dragElement)
-  controls.appendChild(img)
-})
+      tempContainer.innerHTML = svgText
 
-const blocks = document.getElementsByClassName('rectangleControl')
-Array.from(blocks).forEach(rec => {
-  rec.addEventListener('dragstart', dragElement)
-})
+      // Obtener el elemento SVG
+      const svgElement = tempContainer.querySelector('svg')
+      svgElement.setAttribute("height", RECTANGLE_SIZE_CONTROLS)
 
+      // Cambiar el color de todos los elementos de tipo path en el SVG
+      const paths = svgElement.querySelectorAll('path')
+      paths.forEach(path => {
+        path.setAttribute('fill', color) // Cambiar 'fill' por el color deseado
+      })
+      tempContainer.addEventListener('dragstart', dragElement)
+      tempContainer.id = id
+      tempContainer.draggable = true
+
+      // Insertar el SVG en la sección "controles"
+      document.getElementById('controls').appendChild(tempContainer)
+    })
+    .catch(error => console.error('Error al cargar el SVG:', error))
+}
+
+const createControls = () => {
+  const controls = document.getElementById('controls')
+  TABLES_OPTIONS.forEach(option => {
+    loadAndChangeSVGColor(option.src, option.color, option.id)
+  })
+
+  const blocks = document.getElementsByClassName('rectangleControl')
+  Array.from(blocks).forEach(rec => {
+    rec.addEventListener('dragstart', dragElement)
+  })
+}
+
+createControls()
 fabric.Object.prototype.transparentCorners = false
 const canvas = new fabric.Canvas('c1')
 
@@ -70,21 +105,25 @@ function observe (eventName) {
     ) {
       showSelection(opt)
     }
-    console.log(eventName, opt)
   })
 }
 
 const showSelection = opt => {
   const activeObj = canvas.getActiveObject()
-  if (activeObj.type !== 'group') return
-  const objects = activeObj.getObjects()
-
-  const textObject = objects.find(obj => {
-    console.log(obj.type)
-    return obj.type === 'textbox'
-  })
-
+  const textObject = activeObj.textObject
+  if (!textObject) return
+  const zoneType = activeObj.zoneType
+  if (zoneType === ZONE_TYPE.floor) {
+    activeObj.sendToBack()
+  }
   document.getElementById('myText').value = textObject.text
+}
+
+function getRectBounds (rect) {
+  const coords = rect.getCoords()
+  const topMost = Math.min(coords[0].y, coords[1].y, coords[2].y, coords[3].y)
+  const centerX = rect.getCenterPoint().x
+  return { topMost, centerX }
 }
 
 observe('drop:before')
@@ -94,14 +133,73 @@ observe('dragleave')
 observe('selection:created')
 observe('selection:updated')
 
-// dragElement function called on ondrag event.
 function dragElement (e) {
-  console.log(e.target.id)
-  e.dataTransfer.setData('id', e.target.id) // transfer the "data" i.e. id of the target dragged.
+  e.dataTransfer.setData('id', e.target.id)
 }
-const RECTANGLE_SIZE_CONTROLS = 50
 
-const createRectangle = ({ e, color, type }) => {
+const udpateText = rectangle => {
+  const textObject = rectangle.textObject
+  const { topMost, centerX } = getRectBounds(rectangle)
+
+  textObject.set({
+    left: centerX - textObject.width * 0.5,
+    top: topMost - textObject.fontSize
+  })
+
+  canvas.renderAll()
+}
+const centerTextInSVG = svg => {
+  const textObject = svg.textObject
+  textObject.set({
+    left: svg.getCenterPoint().x - textObject.width * 0.5,
+    top: svg.getCenterPoint().y - textObject.height * 0.5
+  })
+  textObject.bringToFront()
+
+  canvas.renderAll()
+}
+const createSVG = ({ svgURL, color, x, y }) => {
+  fabric.loadSVGFromURL(svgURL, function (objects) {
+    const svg = fabric.util.groupSVGElements(objects, {
+    })
+    console.log(svg.width)
+    svg.set({
+      left: x - svg.width * 0.5,
+      top: y - svg.height * 0.5
+    })
+    svg.getObjects().forEach(function (obj) {
+      if (obj.type === 'path') {
+        obj.set('fill', color)
+        obj.set('stroke', color)
+      }
+    })
+    const text = new fabric.Textbox(counter.toString(), {
+      fontSize: FONT_SIZE_TABLES,
+      textAlign: 'center',
+      selectable: false,
+      fill: 'white'
+    })
+    text.set({
+      left: x - text.width * 0.5,
+      top: y - text.height * 0.5
+    })
+    text.bringToFront()
+    counter++
+    svg.set('textObject', text)
+    svg.on('scaling', function () {
+      centerTextInSVG(svg)
+    })
+    svg.on('moving', function () {
+      centerTextInSVG(svg)
+    })
+    svg.on('rotating', function () {
+      centerTextInSVG(svg)
+    })
+    canvas.add(svg, text)
+  })
+}
+
+const createRectangle = ({ e, color, zoneType, fontSize }) => {
   const rectangle = new fabric.Rect({
     width: RECTANGLE_SIZE_CONTROLS,
     height: RECTANGLE_SIZE_CONTROLS,
@@ -110,62 +208,66 @@ const createRectangle = ({ e, color, type }) => {
     fill: color
   })
 
-  const text = new fabric.Textbox(type + counter.toString(), {
-    fontSize: 10,
+  const text = new fabric.Textbox(zoneType + counter.toString(), {
+    fontSize,
     left: e.layerX - RECTANGLE_SIZE_CONTROLS / 2,
     width: RECTANGLE_SIZE_CONTROLS,
-    top: e.layerY - 10 - RECTANGLE_SIZE_CONTROLS / 2,
-    textAlign: 'center'
+    top: e.layerY - fontSize - RECTANGLE_SIZE_CONTROLS / 2,
+    textAlign: 'center',
+    selectable: false
   })
-  text.set('lockScalingX', true)
   counter++
-  const group = new fabric.Group([rectangle, text])
-  // group.on('scaling', function () {
-  //   console.log('asdasda')
-  //   console.log(group)
-  //   const scaleX = group.scaleX
-  //   const scaleY = group.scaleY
-  //   // Escalar solo el rectángulo
-  //   group.item(1).set({
-  //     scaleX: 1 / scaleX,
-  //     scaleY: 1 / scaleY
-  //   })
+  rectangle.set('textObject', text)
+  rectangle.set('zoneType', zoneType)
 
-  //   group.setCoords()
-  //   canvas.renderAll()
-  // })
-  canvas.add(group)
+  rectangle.on('scaling', function () {
+    udpateText(rectangle)
+  })
+  rectangle.on('moving', function () {
+    udpateText(rectangle)
+  })
+  rectangle.on('rotating', function () {
+    udpateText(rectangle)
+  })
+  canvas.add(rectangle, text)
 }
 
 // dropElement function called on ondrop event.
 function dropElement (e) {
   e.preventDefault()
   const id = e.dataTransfer.getData('id')
-
+  console.log(id)
   if (id === 'area') {
-    createRectangle({ e, color: 'rgba(0, 128, 0, 0.1)', type: 'Area' })
+    createRectangle({
+      e,
+      color: 'rgba(0, 128, 0, 0.1)',
+      zoneType: ZONE_TYPE.area,
+      fontSize: FONT_SIZE_AREA
+    })
     return
   }
   if (id === 'floor') {
-    createRectangle({ e, color: 'rgba(0, 0, 255, 0.1)', type: 'Piso' })
+    createRectangle({
+      e,
+      color: 'rgba(0, 0, 255, 0.1)',
+      zoneType: ZONE_TYPE.floor,
+      fontSize: FONT_SIZE_FLOOR
+    })
     return
   }
-  const imgData = document.getElementById(id)
-  const width = imgData.width
-  const height = imgData.height
 
-  const img = new fabric.Image(imgData, {
-    left: e.layerX - width / 2,
-    top: e.layerY - height / 2
-  })
-  const text = new fabric.Textbox(counter.toString(), {
-    fontSize: 10,
-    left: e.layerX - width / 2,
-    width,
-    top: e.layerY - 5,
-    textAlign: 'center'
-  })
-  counter++
-  const group = new fabric.Group([img, text])
-  canvas.add(group)
+  const data = TABLES_OPTIONS.find((table)=> table.id === id)
+  console.log(data)
+  createSVG({svgURL: data.src, color: data.color,x:e.layerX, y:e.layerY})
+}
+
+const removeSelected = () => {
+  const activeObj = canvas.getActiveObject()
+  const textObject = activeObj.textObject
+  canvas.discardActiveObject()
+  if (textObject) {
+    canvas.remove(textObject)
+    canvas.remove(activeObj)
+  }
+  canvas.renderAll()
 }
